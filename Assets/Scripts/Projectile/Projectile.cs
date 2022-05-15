ed6605eb641c3c633eef4965b09560757835edec
Elias Wickander <elias.wt@hotmail.com>
2022-05-15 22:06:43 +0200

    Commented projectile

--- Assets/Scripts/Projectile/Projectile.cs
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -45,6 +45,10 @@ public class Projectile : MonoBehaviour
         m_velocityLastFrame = m_rigidbody.velocity;
     }
 
+    /// <summary>
+    /// Shoot projectile
+    /// </summary>
+    /// <param name="shooter">Shooter</param>
     public void Shoot(CannonData shooter)
     {
         float travelSpeed = m_data.BaseTravelSpeed * shooter.CurrentPower;
@@ -57,22 +61,37 @@ public class Projectile : MonoBehaviour
         if (other.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
         {
             ContactPoint2D contact = other.GetContact(0);
-            Vector2 contactPoint = contact.point;
-            Vector2 contactNormal = contact.normal;
             
-            Vector2 dirToContactPoint = contactPoint - (Vector2)m_transform.position;
-            dirToContactPoint.Normalize();
-            
-            Vector2 bounceDir = Vector2.Reflect(m_velocityLastFrame.normalized, contactNormal);
-            bounceDir.Normalize();
-            
-            Debug.DrawRay(contactPoint, bounceDir, Color.green, 5);
-
-            m_rigidbody.velocity = bounceDir * m_velocityLastFrame.magnitude;
+            BounceOnSurface(contact);
         }
     }
 
-    public List<Vector2> GetTrajectory(Vector2 pos, Vector2 velocity, int maxSteps)
+    /// <summary>
+    /// Bounce on surface point
+    /// </summary>
+    /// <param name="surfaceContact">Contact of surface</param>
+    private void BounceOnSurface(ContactPoint2D surfaceContact)
+    {
+        Vector2 contactPoint = surfaceContact.point;
+        Vector2 contactNormal = surfaceContact.normal;
+            
+        Vector2 dirToContactPoint = contactPoint - (Vector2)m_transform.position;
+        dirToContactPoint.Normalize();
+            
+        Vector2 bounceDir = Vector2.Reflect(m_velocityLastFrame.normalized, contactNormal);
+        bounceDir.Normalize();
+            
+        m_rigidbody.velocity = bounceDir * m_velocityLastFrame.magnitude;
+    }
+    
+    /// <summary>
+    /// Get trajectory of projectile
+    /// </summary>
+    /// <param name="pos">Start position</param>
+    /// <param name="velocity">Projectile velocity</param>
+    /// <param name="steps">How many steps to iterate (defines length)</param>
+    /// <returns>Trajectory points</returns>
+    public List<Vector2> GetTrajectory(Vector2 pos, Vector2 velocity, int steps)
     {
         List<Vector2> points = new List<Vector2>();
  
@@ -81,7 +100,7 @@ public class Projectile : MonoBehaviour
         float drag = 1f - timestep * m_rigidbody.drag;
         Vector2 moveStep = velocity * timestep;
  
-        for (int i = 0; i < maxSteps; ++i)
+        for (int i = 0; i < steps; ++i)
         {
             moveStep += gravityAccel;
             moveStep *= drag;
@@ -90,7 +109,7 @@ public class Projectile : MonoBehaviour
             if (CheckIfPointBlocked(pos, moveStep.normalized, out RaycastHit2D hit))
             {
                 Vector2 bounceVector = Vector2.Reflect((moveStep / timestep), hit.normal);
-                List<Vector2> bounceTrajectory = GetTrajectory(pos, bounceVector, maxSteps);
+                List<Vector2> bounceTrajectory = GetTrajectory(pos, bounceVector, steps);
                 
                 points.AddRange(bounceTrajectory);
                 break;
@@ -101,6 +120,13 @@ public class Projectile : MonoBehaviour
         return points;
     }
 
+    /// <summary>
+    /// Check if projectile would hit obstacle in simulated travel
+    /// </summary>
+    /// <param name="position">Simulated position</param>
+    /// <param name="velocity">Simulated velocity</param>
+    /// <param name="hit">Resulting hit</param>
+    /// <returns>Is point blocked</returns>
     private bool CheckIfPointBlocked(Vector2 position, Vector2 velocity, out RaycastHit2D hit)
     {
         float radius = m_collider.bounds.extents.x;
@@ -110,6 +136,9 @@ public class Projectile : MonoBehaviour
         return hit.collider != null;
     }
 
+    /// <summary>
+    /// Destroys projectile
+    /// </summary>
     public void Destroy()
     {
         Destroy(gameObject);
